21ba268bbd1bffd83161f61081b96df20d919e55
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-19 08:59:51 -0400

    Initial Commit

---
+++ PetUS/PetUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetUSApp.swift
+//  PetUS
+//
+//  Created by Gustavo Anjos on 19/01/21.
+//
+
+import SwiftUI
+
+@main
+struct PetUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
