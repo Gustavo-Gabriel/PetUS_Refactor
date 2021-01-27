b3816a72feb1c106a6fa3b56480b8810aa453eb8
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2021-01-26 23:32:06 -0400

    Feat: Create GridLayout

--- PetUS/view/FavoritesPet.swift
+++ PetUS/view/FavoritesPet.swift
@@ -14,17 +14,27 @@ struct FavoritesPets: View {
     let pets = Pet.all
     
     var body: some View {
+        ZStack{
+            VStack{
+                HeadeView()
+                Spacer()
             ScrollView{
+            
                 LazyVGrid(columns: [GridItem(.flexible(minimum: 50, maximum: 150)), GridItem(.flexible(minimum: 50, maximum: 150))]) {
                     ForEach(0..<pets.count){ index in
                         VStack{
+                            VStack{
                             Image(pets[index].picture).resizable().aspectRatio(contentMode: .fit).clipShape(Circle()).padding()
                             Text(pets[index].name).font(.body)
-                        }.background(Color.blue).cornerRadius(30)
+                            }.background(Color("fundoCell")).cornerRadius(30)
+                        }.background(RoundedRectangle(cornerRadius: 30.0).stroke(Color("border"), lineWidth: 5.0))//.cornerRadius(30)//.cornerRadius(30).border(Color("border"), width: 3)//.fill(Color.green))//)//)//.clipShape(Rectangle())()
                     }
-                }
+                }.padding(.top,30)
+                //.background(LinearGradient(gradient: Gradient(colors: [Color("Back"), Color("BackSec")]), startPoint: .top, endPoint: .bottom))
             
-        }
+            }//.background(LinearGradient(gradient: Gradient(colors: [Color("Back"), Color("BackSec")]), startPoint: .top, endPoint: .bottom))
+            }.background(LinearGradient(gradient: Gradient(colors: [Color("Back"), Color("BackSec")]), startPoint: .top, endPoint: .bottom))
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
